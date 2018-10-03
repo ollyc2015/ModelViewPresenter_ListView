54bf60b8c520a30935d1d98fa5fcc1b881c21e70
Olly <olly_915@hotmail.com>
2018-10-03 18:24:13 -0500

    Started to implement Dagger2 (Design pattern for dependency injection)

--- app/src/main/java/uk/co/oliverbcurtis/ModelViewPresenter_Listview/dagger/AppModule.java
+++ app/src/main/java/uk/co/oliverbcurtis/ModelViewPresenter_Listview/dagger/AppModule.java
@@ -22,7 +22,7 @@ public class AppModule {
 
     private final DaggerApplication application;
 
-    //This is where context is being defined
+    //This is where context is being defined, singleton behaviour is specified as we only need once instance to be created
     public AppModule(DaggerApplication app){
         this.application = app;
     }
@@ -34,17 +34,19 @@ public class AppModule {
         return application;
     }
 
+    //Below creates reference to a new ListViewPresenter object
     @Provides
-    public ListViewPresenter providesListviewPresenter() {
+    public ListViewPresenter providesListViewPresenter() {
         return new ListViewPresenter();
     }
 
+    //Below provides an empty ArrayList so that providesMealListAdapter can be initialised
     @Provides
     public List<Meal> providesMealList() {
         return new ArrayList<>();
     }
 
-
+    //This initialises the MealListAdapter, passing the variables needed to the constructor
     @Provides
     public MealListAdapter providesMealListAdapter(List<Meal> meal) {
         return new MealListAdapter(application, meal);
